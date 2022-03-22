2ec49f4f1990cfb3e8af4ff1d61e04d59cdb57b3
Dominic Yeboah <yeboahd24@gmail.com>
2022-03-22 19:44:53 -0400

    settings modify

--- DisneyPlusClone/settings.py
+++ DisneyPlusClone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,3 +145,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
